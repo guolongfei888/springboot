b9e413d879837ed372e90818d98665c7ebf143aa
guolongfei <guolongfei@adpanshi.com>
2020-03-30 16:02:19 +0800

    jdbc 整合druid

---
+++ springboot-jdbc/springboot-jdbc-druid/src/main/java/com/panshi/springbootjdbcdruid/config/DruidConfig.java
@@ -0,0 +1,62 @@
+package com.panshi.springbootjdbcdruid.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName DruidConfig
+ * @Description
+ * @Author guolongfei
+ * @Date 2020/3/30  15:19
+ * @Version
+ */
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return  new DruidDataSource();
+    }
+
+    // 配置Druid的监控
+    // 1. 配置一个管理后台的Servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("loginUsername","admin");
+        initParams.put("loginPassword","123456");
+        initParams.put("allow","");// 默认就是允许所有的访问
+        initParams.put("deny","192.168.15.21");
+
+        registrationBean.setInitParameters(initParams);
+        return registrationBean;
+    }
+
+    // 2. 配置一个监控的filter
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("exclusions","*.js,*.css,/druid/*");
+
+        bean.setInitParameters(initParams);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+
+        return bean;
+    }
+}
---
+++ springboot-jdbc/springboot-jdbc-druid/src/main/java/com/panshi/springbootjdbcdruid/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.panshi.springbootjdbcdruid.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName HelloController
+ * @Description
+ * @Author guolongfei
+ * @Date 2020/3/30  14:57
+ * @Version
+ */
+@Controller
+public class HelloController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String,Object> map() {
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from employee");
+        return list.get(0);
+    }
+}
