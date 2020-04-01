a140a958e18cb5a7078745973fbf9b62cd8590d8
guolongfei <guolongfei@adpanshi.com>
2020-04-01 15:21:38 +0800

    redis缓存

---
+++ springboot-cache/src/main/java/com/panshi/springbootcache/config/MyRedisConfig.java
@@ -0,0 +1,31 @@
+package com.panshi.springbootcache.config;
+
+import com.panshi.springbootcache.bean.Employee;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.net.UnknownHostException;
+
+/**
+ * @ClassName MyRedisConfig
+ * @Description
+ * @Author guolongfei
+ * @Date 2020/4/1  10:59
+ * @Version
+ */
+@Configuration
+public class MyRedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Employee> empRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Employee> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<Employee> ser = new Jackson2JsonRedisSerializer<Employee>(Employee.class);
+        template.setDefaultSerializer(ser);
+        return template;
+    }
+}
--- springboot-cache/src/test/java/com/panshi/springbootcache/SpringbootCacheApplicationTests.java
+++ springboot-cache/src/test/java/com/panshi/springbootcache/SpringbootCacheApplicationTests.java
@@ -1,13 +1,42 @@
 package com.panshi.springbootcache;
 
+import com.panshi.springbootcache.bean.Employee;
+import com.panshi.springbootcache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootCacheApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    @Autowired
+    RedisTemplate<Object, Employee> empRedisTemplate;
 
     @Test
     void contextLoads() {
+//        stringRedisTemplate.opsForValue().append("msg","hengheng");
+
+//        stringRedisTemplate.opsForList().leftPush("mylist","1");
+//        stringRedisTemplate.opsForList().leftPush("mylist","2");
+
+        Employee emp = employeeMapper.getEmpById(1);
+        // 默认如果保存对象,使用jdk序列化机制,序列化后的数据保存到redis中
+//        redisTemplate.opsForValue().set("emp-01",emp);
+        // 1. 蒋书记以json的方式保存
+        // (1) 自己讲对象转为json
+        // (2) redisTemplate 默认的序列号规则
+        empRedisTemplate.opsForValue().set("emp-02",emp);
     }
 
 }
