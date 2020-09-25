6fba6f2ccc275bb4f5b9c509dab4d25a7cbbe4ad
guolongfei <18255599540@163.com>
2020-09-25 17:43:54 +0800

    自定义starter

---
+++ auto-spring-boot-starter/src/main/java/com/panshi/autospringbootstarter/AutoSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.panshi.autospringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoSpringBootStarterApplication.class, args);
+    }
+
+}
